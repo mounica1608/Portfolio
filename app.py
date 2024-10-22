8bfae7254f73169c5b90ef2a5975d8cb00707e3f
mounica1608 <mounica.patnana@gmail.com>
2024-10-22 19:52:56 +0530

    Resume template

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("resume.html")
+
+
+if __name__ == '__main__':
+    app.run(debug = True,use_reloader=True)       
+
+ 
